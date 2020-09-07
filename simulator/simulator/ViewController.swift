1dee616e0bb6fa3946d05749790fad050e2fdf69
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2020-09-07 21:50:23 +0300

    Search Textfield and Languages

---
+++ simulator/simulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simulator
+//
+//  Created by Sultan MAhfouz on 05/01/1442 AH.
+//  Copyright © 1442 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ simulator/simulatorTests/simulatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  simulatorTests.swift
+//  simulatorTests
+//
+//  Created by Sultan MAhfouz on 05/01/1442 AH.
+//  Copyright © 1442 Udacity. All rights reserved.
+//
+
+import XCTest
+@testable import simulator
+
+class simulatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
